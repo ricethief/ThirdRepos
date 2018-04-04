572d415c7e5abfdf48b936080ae74e4176889eb1
SeChang <electrixocketaus@gmail.com>
2018-04-04 13:03:33 +0930

    loading MyInitial

---
+++ MyInitial/TicketPrice.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ticketing
+{
+    public class TicketPrice
+    {
+        private int section;
+        private int quantity;
+        private bool discount;
+        private decimal amountDue;
+        private decimal mPrice;
+        private decimal discountb;
+
+        const decimal mdecBalcony = 35.5m;
+        const decimal mdecGeneral = 28.75m;
+        const decimal mdecBox = 62.0m;
+       /// const decimal mdecDiscount = 5.0m;
+
+        /// decimal changedDiscount 
+
+
+
+        private int Section
+        {
+            get { return section; }
+            set { section = value; }
+        }
+
+         private int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+         private bool Discount
+        {
+            get { return discount; }
+            set { discount = value; }
+        }
+
+         public decimal AmountDue
+        {
+            get { return amountDue; }
+            set { amountDue = value; }
+        }
+
+        private decimal DiscountB
+        {
+            get { return discountb; }
+            set { discountb = value; }
+        }
+   
+        
+
+        // Constructor for TcicketPrice
+        public TicketPrice(int section, int quantity, bool discount, decimal discountb)
+    {
+        Section = section;
+        Quantity = quantity;
+        Discount = discount;
+        AmountDue = amountDue;
+            DiscountB = discountb;
+    }
+
+       
+     public void calculatePrice()
+     {
+
+         switch (section)
+         {
+             case 1:
+                 mPrice = mdecBalcony;
+                 break;
+             case 2:
+                 mPrice = mdecGeneral;
+                 break;
+             case 3:
+                 mPrice = mdecBox;
+                 break;
+         }
+         if (discount )
+         { mPrice -= discountb; }
+
+         
+         AmountDue = mPrice * quantity;
+
+     }
+    }
+}
---
+++ MyInitial/TicketsForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ticketing
+{
+    public partial class TicketsForm : Form
+    {
+        TicketPrice mTicketPrice;
+        int mSection = 2;
+        int mQuantity = 0;
+        bool mDiscount;
+        public decimal discountb;
+
+        public TicketsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TicketsForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void chkDiscount_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (chkDiscount.Checked)
+            { txtDiscount.Enabled = true; }
+            else { txtDiscount.Enabled = false; }
+        }
+
+        private void cmdCalculate_Click(object sender, EventArgs e)
+        {
+            mQuantity = int.Parse(txtQuantity.Text);
+
+            if (chkDiscount.Checked)
+                { mDiscount = true;
+                txtDiscount.Enabled = true;
+                }
+
+            if (radBalcony.Checked)
+                { mSection = 1;}
+            if (radGeneral.Checked)
+                { mSection = 2;}
+            if (radBox.Checked)
+                { mSection = 3; }
+
+            discountb = decimal.Parse(txtDiscount.Text);
+            
+
+
+            mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount, discountb);
+
+            mTicketPrice.calculatePrice();
+            lblAmount.Text = System.Convert.ToString(mTicketPrice.AmountDue);
+        }
+
+      
+    }
+}
